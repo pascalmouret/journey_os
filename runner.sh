924f63bece1c2a257517bc0bb63e64675d247ec4
Pascal Mouret <pascal.mouret@me.com>
2022-01-24 21:58:06 +0100

    Project: finally chose a name for the OS!
    Since I was inspired by Serenty OS to get back into hobby OS
    development, it feel fitting to chose a name in a similar vain.
    
    As such, this is now Journey OS. I decided on the name since for
    this project I'm more interested in the journey of of learning
    while building than ever having a piece of software that's
    useful.
    
    It's all about the learning and all the bugs we make along the
    way.

--- runner.sh
+++ runner.sh
@@ -2,12 +2,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SUCCESS_CODE=85 # QEMU shifts exit code by one
 
 mkdir -p $SCRIPT_DIR/target/isodir/boot/grub
-cp $1 $SCRIPT_DIR/target/isodir/boot/rustos.bin
+cp $1 $SCRIPT_DIR/target/isodir/boot/journey_os.bin
 cp $SCRIPT_DIR/grub.cfg $SCRIPT_DIR/target/isodir/boot/grub/grub.cfg
-grub-mkrescue -o $SCRIPT_DIR/target/rustos.iso $SCRIPT_DIR/target/isodir
+grub-mkrescue -o $SCRIPT_DIR/target/journey_os.iso $SCRIPT_DIR/target/isodir
 
 qemu-system-x86_64 \
-  -cdrom $SCRIPT_DIR/target/rustos.iso \
+  -cdrom $SCRIPT_DIR/target/journey_os.iso \
   -device VGA \
   -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
   -serial stdio
